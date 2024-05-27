efe4a698f18961c4ce28bc63988e81e7beb849d3
madaooftheblues <azlanrasool@gmail.com>
2024-05-27 18:44:15 +0500

    test: Add test to cater empty strings

--- capitalize.test.js
+++ capitalize.test.js
@@ -7,4 +7,12 @@ test('module exists', () => {
 test('Capitalize a string', () => {
     expect(capitalize('azlan')).toBe('Azlan')
     expect(capitalize('lion')).toBe('Lion')
+    expect(capitalize('the man who sold the world.')).toBe(
+        'The man who sold the world.',
+    )
+    expect(capitalize('Brilliant diagnostics')).toBe('Brilliant diagnostics')
+})
+
+test('Check for empty string', () => {
+    expect(capitalize('')).toBe('')
 })
